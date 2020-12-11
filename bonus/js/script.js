cc3f5713429405cc7203c00356fdd8e3cb276a64
andrea <breezer087@gmail.com>
2020-12-11 16:27:25 +0100

    simple array

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,18 +7,27 @@
 
 //Trying number input validation
 
-var name = prompt('Inserisci Nome');
-if (name === '') {
-    alert('Inserisci un nome valido');
-} else {
-    var name = name;
-}
-var age = prompt('Inserisci età');
-if (age === '') {
-    alert('Il campo è vuoto');
-} else if (isNaN(age)) {
-    alert('Inserisci età valida');  //isNaN(variable) 
-} else
-var age = age;
-var output = name + ' ' + age;
-document.getElementById('result2').innerText = output;
+// var name = prompt('Inserisci Nome');
+// if (name === '') {
+//     alert('Il campo è vuoto');
+// } else {
+//     var name = name;
+// }
+// var age = prompt('Inserisci età');
+// if (age === '') {
+//     alert('Il campo è vuoto');
+// } else if (isNaN(age)) {
+//     alert('Inserisci età valida');  //isNaN(variable)
+// } else
+// var age = age;
+// var output = name + ' ' + age;
+// document.getElementById('result2').innerText = output; //null if empty
+
+// Trying arrays
+
+var output = [
+    prompt('Inserisci il tuo nome'),
+    prompt('Inserisci la tua età'),
+    prompt('Inserisci il tuo colore preferito')
+];
+document.getElementById('result3').innerText = output;
